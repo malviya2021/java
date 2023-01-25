e8073bc17d0b30703711a3a79a85e8a689b30088
malviya2021 <87021520+malviya2021@users.noreply.github.com>
2023-01-25 16:52:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
-      // Prints "Hello, I am here in the terminal window.
+      // Prints "Hello, I am here in the terminal window. i m devops student.
       System.out.println("Hello all, Shahataaz and Yogesh");
    }
 }
